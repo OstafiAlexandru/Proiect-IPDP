91c809c87c3e412d77f253187048d1ff45a54b4e
Ostafi Alexandru <61746596+OstafiAlexandru@users.noreply.github.com>
2020-06-26 17:53:51 +0300

    Add files via upload

---
+++ ProiectIPDP/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ProiectIPDP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkConnection = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkConnection
+            // 
+            this.checkConnection.AutoSize = true;
+            this.checkConnection.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkConnection.Location = new System.Drawing.Point(65, 30);
+            this.checkConnection.Name = "checkConnection";
+            this.checkConnection.Size = new System.Drawing.Size(60, 24);
+            this.checkConnection.TabIndex = 0;
+            this.checkConnection.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkConnection);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label checkConnection;
+    }
+}
+
---
+++ ProiectIPDP/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ProiectIPDP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                OleDbConnection connection = new OleDbConnection();
+                connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=LogInInfo.accdb;Persist Security Info=False;";
+                connection.Open();
+                checkConnection.Text = "Connection Successful";
+                connection.Close();
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+
+            }
+        }
+    }
+}
